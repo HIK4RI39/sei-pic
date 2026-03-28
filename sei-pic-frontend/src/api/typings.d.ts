5992861ad1f2ae8dcda0fa21213d55b3ca409e7f
HIK4RI39 <14401191+hik4ri39@user.noreply.gitee.com>
2026-03-28 15:32:20 +0800

    前端 openapi生成接口

--- sei-pic-frontend/src/api/typings.d.ts
+++ sei-pic-frontend/src/api/typings.d.ts
@@ -1,7 +1,111 @@
 declare namespace API {
-  type BaseResponseObject_ = {
+  type BaseResponseBoolean_ = {
     code?: number
-    data?: Record<string, any>
+    data?: boolean
     message?: string
+  }
+
+  type BaseResponseLong_ = {
+    code?: number
+    data?: number
+    message?: string
+  }
+
+  type BaseResponsePageUserVO_ = {
+    code?: number
+    data?: PageUserVO_
+    message?: string
+  }
+
+  type BaseResponseUser_ = {
+    code?: number
+    data?: User
+    message?: string
+  }
+
+  type BaseResponseUserVO_ = {
+    code?: number
+    data?: UserVO
+    message?: string
+  }
+
+  type IdRequest = {
+    id?: number
+  }
+
+  type PageUserVO_ = {
+    current?: number
+    pages?: number
+    records?: UserVO[]
+    size?: number
+    total?: number
+  }
+
+  type User = {
+    createTime?: string
+    editTime?: string
+    id?: number
+    isDelete?: number
+    updateTime?: string
+    userAccount?: string
+    userAvatar?: string
+    userName?: string
+    userPassword?: string
+    userProfile?: string
+    userRole?: string
+  }
+
+  type UserAddRequest = {
+    userAccount?: string
+  }
+
+  type UserEditRequest = {
+    userName?: string
+    userPassword?: string
+    userProfile?: string
+  }
+
+  type UserLoginRequest = {
+    userAccount?: string
+    userPassword?: string
+  }
+
+  type UserPageRequest = {
+    createBeginTime?: string
+    createEndTime?: string
+    current?: number
+    id?: number
+    pageSize?: number
+    sortField?: string
+    sortOrder?: string
+    userAccount?: string
+    userName?: string
+    userRole?: string
+  }
+
+  type UserRegisterRequest = {
+    checkPassword?: string
+    userAccount?: string
+    userPassword?: string
+  }
+
+  type UserUpdateRequest = {
+    id?: number
+    userName?: string
+    userPassword?: string
+    userProfile?: string
+    userRole?: string
+  }
+
+  type UserVO = {
+    createTime?: string
+    editTime?: string
+    id?: number
+    updateTime?: string
+    userAccount?: string
+    userAvatar?: string
+    userName?: string
+    userProfile?: string
+    userRole?: string
   }
 }
--- sei-pic-frontend/src/api/userController.ts
+++ sei-pic-frontend/src/api/userController.ts
@@ -2,10 +2,144 @@
 /* eslint-disable */
 import request from '@/request'
 
-/** health GET /api/user/health */
-export async function healthUsingGet(options?: { [key: string]: any }) {
-  return request<API.BaseResponseObject_>('/api/user/health', {
+/** addUser POST /api/user/add */
+export async function addUserUsingPost(body: API.UserAddRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseLong_>('/api/user/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** deleteUserById POST /api/user/delete */
+export async function deleteUserByIdUsingPost(
+  body: API.IdRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/api/user/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** editUser POST /api/user/edit */
+export async function editUserUsingPost(
+  body: API.UserEditRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/api/user/edit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** getUserById POST /api/user/get */
+export async function getUserByIdUsingPost(body: API.IdRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseUser_>('/api/user/get', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** getLoginUser GET /api/user/get/login */
+export async function getLoginUserUsingGet(options?: { [key: string]: any }) {
+  return request<API.BaseResponseUserVO_>('/api/user/get/login', {
     method: 'GET',
+    ...(options || {}),
+  })
+}
+
+/** getUserVoById POST /api/user/get/vo */
+export async function getUserVoByIdUsingPost(
+  body: API.IdRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseUserVO_>('/api/user/get/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** listUserVoByPage POST /api/user/list/page/vo */
+export async function listUserVoByPageUsingPost(
+  body: API.UserPageRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageUserVO_>('/api/user/list/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** login POST /api/user/login */
+export async function loginUsingPost(body: API.UserLoginRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseUserVO_>('/api/user/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** logout GET /api/user/logout */
+export async function logoutUsingGet(options?: { [key: string]: any }) {
+  return request<API.BaseResponseBoolean_>('/api/user/logout', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
+
+/** register POST /api/user/register */
+export async function registerUsingPost(
+  body: API.UserRegisterRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseLong_>('/api/user/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** updateUser POST /api/user/update */
+export async function updateUserUsingPost(
+  body: API.UserUpdateRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/api/user/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
     ...(options || {}),
   })
 }
