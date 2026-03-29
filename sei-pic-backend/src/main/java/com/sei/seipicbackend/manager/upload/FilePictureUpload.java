9b99cf0cb70e0d091a9952f74df9462968203c6f
HIK4RI39 <14401191+hik4ri39@user.noreply.gitee.com>
2026-03-29 23:06:45 +0800

    后端 模版方法设计模式优化图片上传, 根据文件或url上传

--- sei-pic-backend/src/main/java/com/sei/seipicbackend/manager/upload/FilePictureUpload.java
+++ sei-pic-backend/src/main/java/com/sei/seipicbackend/manager/upload/FilePictureUpload.java
@@ -16,6 +16,8 @@ import java.util.List;
  */
 @Service
 public class FilePictureUpload extends PictureUploadTemplate {
+    final long ONE_M = 1024 * 1024L;
+    final List<String> ALLOW_FORMAT_LIST = Arrays.asList("jpeg", "jpg", "png", "webp");
 
     @Override
     protected void validPicture(Object inputSource) {
@@ -23,12 +25,10 @@ public class FilePictureUpload extends PictureUploadTemplate {
         ThrowUtils.throwIf(multipartFile == null, ErrorCode.PARAMS_ERROR, "文件不能为空");
         // 1. 校验文件大小
         long fileSize = multipartFile.getSize();
-        final long ONE_M = 1024 * 1024L;
         ThrowUtils.throwIf(fileSize > 2 * ONE_M, ErrorCode.PARAMS_ERROR, "文件大小不能超过 2M");
         // 2. 校验文件后缀
         String fileSuffix = FileUtil.getSuffix(multipartFile.getOriginalFilename());
         // 允许上传的文件后缀
-        final List<String> ALLOW_FORMAT_LIST = Arrays.asList("jpeg", "jpg", "png", "webp");
         ThrowUtils.throwIf(!ALLOW_FORMAT_LIST.contains(fileSuffix), ErrorCode.PARAMS_ERROR, "文件类型错误");
     }
 
