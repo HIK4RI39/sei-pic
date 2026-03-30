cf427ea7acaf3a45271a98b16566ff219922491f
HIK4RI39 <staylazy@163.com>
2026-03-31 06:59:20 +0800

    🎨 侧边栏

--- sei-pic-frontend/src/layouts/BasicLayout.vue
+++ sei-pic-frontend/src/layouts/BasicLayout.vue
@@ -1,41 +1,53 @@
 <template>
     <div id="basicLayout">
-        <a-layout style="min-height: 100vh;">
+        <a-layout style="min-height: 100vh">
             <a-layout-header class="header">
                 <GlobalHeader />
             </a-layout-header>
-            <a-layout-content class="content">
-                <router-view />
-            </a-layout-content>
+            <a-layout>
+                <GlobalSider class="sider" />
+                <a-layout-content class="content">
+                    <router-view />
+                </a-layout-content>
+            </a-layout>
             <a-layout-footer class="footer">
-                <a href="https://github.com/HIK4RI39" target="_blank">
-                    sei图库 By Sei
-                </a>
+                <a href="https://www.codefather.cn" target="_blank"> 编程导航 by 程序员鱼皮 </a>
             </a-layout-footer>
         </a-layout>
     </div>
 </template>
 
 <script setup lang="ts">
-import GlobalHeader from '@/components/GlobalHeader.vue';
-
+import GlobalHeader from '@/components/GlobalHeader.vue'
+import GlobalSider from "@/components/GlobalSider.vue";
 </script>
 
 <style scoped>
-#basicLayout header {
+#basicLayout .header {
+    padding-inline: 20px;
     background: white;
     color: unset;
-    margin-bottom: 16px;
-    padding-inline: 20px;
+    margin-bottom: 1px;
+}
+
+#basicLayout .sider {
+    background: #fff;
+    border-right: 0.5px solid #eee;
+    padding-top: 20px;
+}
+
+#basicLayout :deep(.ant-menu-root) {
+    border-bottom: none !important;
+    border-inline-end: none !important;
 }
 
 #basicLayout .content {
-    padding: 20px;
-    backdrop-filter: linear-gradient(to right, #fefefe, #fff);
+    padding: 28px;
+    background: linear-gradient(to right, #fefefe, #fff);
     margin-bottom: 28px;
 }
 
-#basicLayOut .footer {
+#basicLayout .footer {
     background: #efefef;
     padding: 16px;
     position: fixed;
