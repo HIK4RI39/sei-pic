b9637d7d7fca88d285ffe9cb408c8f3eadbac1f8
HIK4RI39 <14401191+hik4ri39@user.noreply.gitee.com>
2026-03-31 01:42:24 +0800

    ⚙️ 空间模块: 实现创建空间

---
+++ sei-pic-backend/src/main/java/com/sei/seipicbackend/controller/SpaceController.java
@@ -0,0 +1,43 @@
+package com.sei.seipicbackend.controller;
+
+import cn.hutool.core.util.ObjUtil;
+import com.sei.seipicbackend.common.BaseResponse;
+import com.sei.seipicbackend.common.ResponseUtils;
+import com.sei.seipicbackend.exception.ErrorCode;
+import com.sei.seipicbackend.exception.ThrowUtils;
+import com.sei.seipicbackend.model.dto.space.SpaceAddRequest;
+import com.sei.seipicbackend.service.SpaceService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author hikari39_
+ * @since 2026-03-31
+ */
+@RestController
+@RequestMapping("/space")
+public class SpaceController {
+    @Resource
+    private SpaceService spaceService;
+
+    // region -------------------------- 管理员 --------------------------
+
+    // endregion
+
+    // region -------------------------- 用户 --------------------------
+    @PostMapping("/create")
+    public BaseResponse<Boolean> createSpace(@RequestBody SpaceAddRequest spaceAddRequest, HttpServletRequest request) {
+        ThrowUtils.throwIf(ObjUtil.isNull(spaceAddRequest), ErrorCode.PARAMS_ERROR);
+        boolean result = spaceService.createSpace(spaceAddRequest, request);
+        return ResponseUtils.success(result);
+    }
+    // endregion
+
+
+
+}
---
+++ sei-pic-backend/src/main/java/com/sei/seipicbackend/service/impl/SpaceServiceImpl.java
@@ -0,0 +1,137 @@
+package com.sei.seipicbackend.service.impl;
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sei.seipicbackend.constant.UserConstant;
+import com.sei.seipicbackend.exception.BusinessException;
+import com.sei.seipicbackend.exception.ErrorCode;
+import com.sei.seipicbackend.exception.ThrowUtils;
+import com.sei.seipicbackend.model.dto.space.SpaceAddRequest;
+import com.sei.seipicbackend.model.enums.SpaceLevelEnum;
+import com.sei.seipicbackend.model.pojo.Space;
+import com.sei.seipicbackend.model.vo.UserVO;
+import com.sei.seipicbackend.service.SpaceService;
+import com.sei.seipicbackend.mapper.SpaceMapper;
+import com.sei.seipicbackend.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+* @author hikari39
+* @description 针对表【space(空间)】的数据库操作Service实现
+* @createDate 2026-03-31 01:00:14
+*/
+@Service
+public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space>
+    implements SpaceService{
+
+    @Resource
+    private UserService userService;
+
+    // region -------------------------- 公共公共方法 --------------------------
+
+    /**
+     * 校验空间
+     * 校验名称, 级别是否合法/存在
+     * @param space
+     * @param add
+     */
+    @Override
+    public void validSpace(Space space, boolean add) {
+        ThrowUtils.throwIf(space == null, ErrorCode.PARAMS_ERROR);
+        // 从对象中取值
+        String spaceName = space.getSpaceName();
+        Integer spaceLevel = space.getSpaceLevel();
+        SpaceLevelEnum spaceLevelEnum = SpaceLevelEnum.getEnumByValue(spaceLevel);
+        // 创建
+        if (add) {
+            if (StrUtil.isBlank(spaceName)) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR, "空间名称不能为空");
+            }
+            if (spaceLevel == null) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR, "空间级别不能为空");
+            }
+        }
+        // 修改数据时，如果要改空间级别
+        if (spaceLevel != null && spaceLevelEnum == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "空间级别不存在");
+        }
+        if (StrUtil.isNotBlank(spaceName) && spaceName.length() > 30) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "空间名称过长");
+        }
+    }
+
+    /**
+     * 创建或更新空间时, 根据空间级别自动填充限额等数据
+     * @param space
+     */
+    @Override
+    public void fillSpaceBySpaceLevel(Space space) {
+        // 根据空间级别，自动填充限额
+        SpaceLevelEnum spaceLevelEnum = SpaceLevelEnum.getEnumByValue(space.getSpaceLevel());
+        if (spaceLevelEnum != null) {
+            long maxSize = spaceLevelEnum.getMaxSize();
+            if (space.getMaxSize() == null) {
+                space.setMaxSize(maxSize);
+            }
+            long maxCount = spaceLevelEnum.getMaxCount();
+            if (space.getMaxCount() == null) {
+                space.setMaxCount(maxCount);
+            }
+        }
+    }
+
+    // endregion
+
+
+    // region -------------------------- 管理员 --------------------------
+
+    // endregion
+
+    // region -------------------------- 用户 --------------------------
+
+    /**
+     * 创建空间
+     * @param spaceAddRequest
+     * @param request
+     * @return
+     */
+    @Override
+    public boolean createSpace(SpaceAddRequest spaceAddRequest, HttpServletRequest request) {
+        String spaceName = spaceAddRequest.getSpaceName();
+        Integer spaceLevel = spaceAddRequest.getSpaceLevel();
+        Space space = new Space();
+        space.setSpaceName(spaceName);
+        space.setSpaceLevel(spaceLevel);
+        // 校验空间
+        validSpace(space, true);
+        // 用户仅能创建基础类型空间
+        UserVO loginUser = userService.getLoginUser(request);
+        String userRole = loginUser.getUserRole();
+        if (UserConstant.DEFAULT_ROLE.equals(userRole) && SpaceLevelEnum.COMMON.getValue()!=spaceLevel) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        // 填写空间参数
+        fillSpaceBySpaceLevel(space);
+        // 不能重复创建空间
+        boolean exists = lambdaQuery().eq(Space::getUserId, loginUser.getId()).exists();
+        ThrowUtils.throwIf(exists, ErrorCode.OPERATION_ERROR, "最多创建1个空间");
+        // 填写userId
+        space.setUserId(loginUser.getId());
+
+        boolean result = save(space);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR, "空间创建失败");
+        return true;
+    }
+
+    // endregion
+
+
+
+}
+
+
+
+
