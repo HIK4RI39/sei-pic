91c8affd1d4247826fa6ab36c9147a377c27dea4
HIK4RI39 <staylazy@163.com>
2026-04-03 20:50:21 +0800

    ⚙️ sql建表语句整合

--- sei-pic-backend/src/main/resources/sql/create_table.sql
+++ sei-pic-backend/src/main/resources/sql/create_table.sql
@@ -1,51 +1,56 @@
 -- 用户表
 create table if not exists user
 (
-    id           bigint auto_increment comment 'id' primary key,
-    userAccount  varchar(256)                           not null comment '账号',
-    userPassword varchar(512)                           not null comment '密码',
-    userName     varchar(256)                           null comment '用户昵称',
-    userAvatar   varchar(1024)                          null comment '用户头像',
-    userProfile  varchar(512)                           null comment '用户简介',
-    userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin',
-    editTime     datetime     default CURRENT_TIMESTAMP not null comment '编辑时间',
-    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint      default 0                 not null comment '是否删除',
+    id               bigint auto_increment comment 'id' primary key,
+    userAccount      varchar(256)                           not null comment '账号',
+    userPassword     varchar(512)                           not null comment '密码',
+    userName         varchar(256)                           null comment '用户昵称',
+    userAvatar       varchar(1024)                          null comment '用户头像',
+    userProfile      varchar(512)                           null comment '用户简介',
+    userRole         varchar(256) default 'user'            not null comment '用户角色：user/admin/vip',
+    vipExpiredTime   datetime                               null comment '会员过期时间',
+    vipCode          varchar(128)                           null comment '会员兑换码',
+    vipNumber        bigint                                 null comment '会员编号',
+    editTime         datetime     default CURRENT_TIMESTAMP not null comment '编辑时间',
+    createTime       datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime       datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete         tinyint      default 0                 not null comment '是否删除',
     UNIQUE KEY uk_userAccount (userAccount),
     INDEX idx_userName (userName)
 ) comment '用户' collate = utf8mb4_unicode_ci;
 
--- 用户表添加新列, 会员相关功能
-ALTER TABLE user
-    ADD COLUMN vipExpiredTime datetime null comment '会员过期时间',
-    ADD COLUMN vipCode varchar(128) null comment '会员兑换码',
-    ADD COLUMN vipNumber bigint null comment '会员编号';
-
 -- 图片表
 create table if not exists picture
 (
-    id           bigint auto_increment comment 'id' primary key,
-    url          varchar(512)                       not null comment '图片 url',
-    name         varchar(128)                       not null comment '图片名称',
-    introduction varchar(512)                       null comment '简介',
-    category     varchar(64)                        null comment '分类',
-    tags         varchar(512)                      null comment '标签（JSON 数组）',
-    picSize      bigint                             null comment '图片体积',
-    picWidth     int                                null comment '图片宽度',
-    picHeight    int                                null comment '图片高度',
-    picScale     double                             null comment '图片宽高比例',
-    picFormat    varchar(32)                        null comment '图片格式',
-    userId       bigint                             not null comment '创建用户 id',
-    createTime   datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    editTime     datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
-    updateTime   datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint  default 0                 not null comment '是否删除',
-    INDEX idx_name (name),                 -- 提升基于图片名称的查询性能
-    INDEX idx_introduction (introduction), -- 用于模糊搜索图片简介
-    INDEX idx_category (category),         -- 提升基于分类的查询性能
-    INDEX idx_tags (tags),                 -- 提升基于标签的查询性能
-    INDEX idx_userId (userId)              -- 提升基于用户 ID 的查询性能
+    id            bigint auto_increment comment 'id' primary key,
+    url           varchar(512)                       not null comment '图片 url',
+    thumbnailUrl  varchar(512)                       null comment '缩略图 url',
+    name          varchar(128)                       not null comment '图片名称',
+    introduction  varchar(512)                       null comment '简介',
+    category      varchar(64)                        null comment '分类',
+    tags          varchar(512)                       null comment '标签（JSON 数组）',
+    picSize       bigint                             null comment '图片体积',
+    picWidth      int                                null comment '图片宽度',
+    picHeight     int                                null comment '图片高度',
+    picScale      double                             null comment '图片宽高比例',
+    picFormat     varchar(32)                        null comment '图片格式',
+    picColor      varchar(16)                        null comment '图片主色调',
+    userId        bigint                             not null comment '创建用户 id',
+    spaceId       bigint                             null comment '空间 id（为空表示公共空间）',
+    reviewStatus  int      default 0                 not null comment '审核状态：0-待审核; 1-通过; 2-拒绝',
+    reviewMessage varchar(512)                       null comment '审核信息',
+    reviewerId    bigint                             null comment '审核人 ID',
+    reviewTime    datetime                           null comment '审核时间',
+    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    editTime      datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    INDEX idx_name (name),
+    INDEX idx_introduction (introduction),
+    INDEX idx_category (category),
+    INDEX idx_tags (tags),
+    INDEX idx_userId (userId),
+    INDEX idx_spaceId (spaceId),
+    INDEX idx_reviewStatus (reviewStatus)
 ) comment '图片' collate = utf8mb4_unicode_ci;
 
 -- 空间表
@@ -54,6 +59,7 @@ create table if not exists space
     id         bigint auto_increment comment 'id' primary key,
     spaceName  varchar(128)                       null comment '空间名称',
     spaceLevel int      default 0                 null comment '空间级别：0-普通版 1-专业版 2-旗舰版',
+    spaceType  int      default 0                 not null comment '空间类型：0-私有 1-团队',
     maxSize    bigint   default 0                 null comment '空间图片的最大总大小',
     maxCount   bigint   default 0                 null comment '空间图片的最大数量',
     totalSize  bigint   default 0                 null comment '当前空间下图片的总大小',
@@ -63,68 +69,24 @@ create table if not exists space
     editTime   datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
     updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete   tinyint  default 0                 not null comment '是否删除',
-    -- 索引设计
-    index idx_userId (userId),        -- 提升基于用户的查询效率
-    index idx_spaceName (spaceName),  -- 提升基于空间名称的查询效率
-    index idx_spaceLevel (spaceLevel) -- 提升按空间级别查询的效率
+    INDEX idx_userId (userId),
+    INDEX idx_spaceName (spaceName),
+    INDEX idx_spaceLevel (spaceLevel),
+    INDEX idx_spaceType (spaceType)
 ) comment '空间' collate = utf8mb4_unicode_ci;
-
-
--- 空间添加新列 空间类型
-ALTER TABLE space
-    ADD COLUMN spaceType int default 0 not null comment '空间类型：0-私有 1-团队';
--- 根据空间类型创建索引
-CREATE INDEX idx_spaceType ON space (spaceType);
-
-
--- 图片表添加审核相关新列
-ALTER TABLE picture
-    ADD COLUMN reviewStatus INT DEFAULT 0 NOT NULL COMMENT '审核状态：0-待审核; 1-通过; 2-拒绝',
-    ADD COLUMN reviewMessage VARCHAR(512) NULL COMMENT '审核信息',
-    ADD COLUMN reviewerId BIGINT NULL COMMENT '审核人 ID',
-    ADD COLUMN reviewTime DATETIME NULL COMMENT '审核时间';
-
--- 图片表添加新列 缩略图
-ALTER TABLE picture
-    ADD COLUMN thumbnailUrl varchar(512) NULL COMMENT '缩略图 url';
-
--- 弃用了图片表的逻辑删除功能
-ALTER TABLE picture
-    DROP COLUMN isDelete;
-
--- 图片表添加新列, 所属的空间id
-ALTER TABLE picture
-    ADD COLUMN spaceId  bigint  null comment '空间 id（为空表示公共空间）';
-
--- 图片表添加新列, 主色调
-ALTER TABLE picture
-    ADD COLUMN picColor varchar(16) null comment '图片主色调';
-
--- 创建索引
-CREATE INDEX idx_spaceId ON picture (spaceId);
-
--- 创建基于 reviewStatus 列的索引
-CREATE INDEX idx_reviewStatus ON picture (reviewStatus);
-
 
 -- 空间成员表
 create table if not exists space_user
 (
-    id         bigint auto_increment comment 'id' primary key,
-    spaceId    bigint                                 not null comment '空间 id',
-    userId     bigint                                 not null comment '用户 id',
-    spaceRole  varchar(128) default 'viewer'          null comment '空间角色：viewer/editor/admin',
-    createTime datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    -- 索引设计
-    UNIQUE KEY uk_spaceId_userId (spaceId, userId), -- 唯一索引，用户在一个空间中只能有一个角色
-    INDEX idx_spaceId (spaceId),                    -- 提升按空间查询的性能
-    INDEX idx_userId (userId)                       -- 提升按用户查询的性能
+    id            bigint auto_increment comment 'id' primary key,
+    spaceId       bigint                                 not null comment '空间 id',
+    userId        bigint                                 not null comment '用户 id',
+    createUserId  bigint                                 null comment '邀请人',
+    spaceRole     varchar(128) default 'viewer'          null comment '空间角色：viewer/editor/admin',
+    confirmStatus int          default 0                 not null comment '邀请确认状态 0-待确认 1-已确认 2-已拒绝',
+    createTime    datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime    datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    UNIQUE KEY uk_spaceId_userId (spaceId, userId),
+    INDEX idx_spaceId (spaceId),
+    INDEX idx_userId (userId)
 ) comment '空间用户关联' collate = utf8mb4_unicode_ci;
-
--- 空间成员添加新列 申请确认状态
-ALTER TABLE space_user
-    ADD COLUMN confirmStatus int default 0 not null comment '邀请确认状态 0-待确认 1-已确认 2-已拒绝';
--- 空间成员添加新列 邀请人
-ALTER TABLE space_user
-    ADD COLUMN createUserId bigint comment '邀请人';
